ef53aced6ffc769ae439bbb0668affa90638845d
Umeshkumar52 <123877527+Umeshkumar52@users.noreply.github.com>
2024-10-10 12:52:16 +0530

    Update axios.js

--- src/helper/axios.js
+++ src/helper/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
  const instance= axios.create({
-    baseURL:"http://localhost:5002/api",
+    baseURL:"https://chat-backend-mmyl.onrender.com/api",
     // baseURL:"https://vercel.com/umeshkumar52s-projects/chat-backend/api",
     headers:{
         "Content-Type":"application/json"
