b7687088b557e197929d95a2ec29eded63f6a464
Umeshkumar52 <123877527+Umeshkumar52@users.noreply.github.com>
2024-10-11 20:10:36 +0530

    Update axios.js

--- src/helper/axios.js
+++ src/helper/axios.js
@@ -3,14 +3,16 @@ import axios from "axios";
     // baseURL:"http://localhost:5002/api",
     baseURL:"https://chat-backend-mmyl.onrender.com/api",
     headers:{
-        "Content-Type":"application/json"
+        "Content-Type":"application/json",
+        "withCredentials":"true"
     }
 })
 export const multiPartInstance= axios.create({
     // baseURL:"http://localhost:5002/api",
     baseURL:"https://chat-backend-mmyl.onrender.com/api",
     headers:{
-        "Content-Type":"multipart/form-data"
+        "Content-Type":"multipart/form-data",
+        "withCredentials":"true"
     }
 })
 export default instance
